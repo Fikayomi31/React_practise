e32fb8fa6dcdde053fab3e6781c987420c15f10d
Fikayomi31 <fikayoogundijo@gmail.com>
2024-12-18 22:35:40 -0800

    debugging

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function Square({ value, onSquareClick }) {
   )  
 }
 
-  export default function Board() {
+  function Board() {
     const [xIsNext, setXIsNext] = useState(true)
     const [squares, setsquares] = useState(Array(9).fill(null));
     
@@ -82,4 +82,26 @@ function calulateWinner(squares) {
       
   }
   return null
+}
+
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true)
+  const [history, setHistory] = useState([Array(9)])
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  return (
+    <div className='game'>
+      <div className='game-board'>
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className='game-info'>
+      <ol>{/*TODO */}</ol>
+      </div>
+    </div>
+  );
 }
